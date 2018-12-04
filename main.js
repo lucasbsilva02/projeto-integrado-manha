de31a5fdef0aaca79a35a1d71cd59feeb4eb26e1
Lucas Batista da Silva <39039061+lucasbsilva02@users.noreply.github.com>
2018-12-04 07:05:28 -0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function openSideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+    /*document.getElementById('block-main').style.marginLeft = '250px';
+    document.getElementById('block-footer').style.marginLeft = '250px';*/
+}/** Inserindo no HTML a largura para o menu */
+/** Agora retirando */
+function closeSideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    /*document.getElementById('block-main').style.marginLeft = '0';
+    document.getElementById('block-footer').style.marginLeft = '0';*/
+}
+/** Maps API */
+
